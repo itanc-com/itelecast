ee3c474a5f2863531cd6476e879782d97e9925a6
Babak Jahangiri <babakjahangiri@gmail.com>
2025-08-01 20:38:27 +0100

    db session manager

---
+++ app/db/session.py
@@ -0,0 +1,18 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.pydantic.settings import settings
+
+from .session_manager import SessionManager
+
+engine_options = {
+    "echo": getattr(settings, "echo_sql", False),
+    "connect_args": {"check_same_thread": False} if settings.database_uri.startswith("sqlite") else {},
+}
+
+sessionmanager = SessionManager(settings.database_uri, engine_options)
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with sessionmanager.session() as session:
+        yield session
