5b4f0c3935fc890a8ab742ed8733d5a83e0934df
Babak Jahangiri <babakjahangiri@gmail.com>
2025-08-01 20:39:00 +0100

    pydantic settings

---
+++ app/core/pydantic/settings.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EnvironmentType(str, Enum):
+    DEVELOPMENT = "development"
+    STAGING = "staging"
+    PRODUCTION = "production"
+    LOCAL = "local"
+    TESTING = "testing"
+    
+class Settings(BaseSettings):
+    environment: EnvironmentType
+    bot_http_api_token: str
+    bot_username: str
+    channel_id: str
+    database_uri: str
+
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_file="./app/.env",
+        env_file_encoding="utf-8",
+    )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings: Settings = get_settings() 
